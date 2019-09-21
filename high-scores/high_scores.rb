047fd62abbd52d1e9f737aef4d43ba7acd419c5a
HenriqueMariano25 <you@example.com>
2019-09-21 06:33:32 -0300

    Refatora o problema 5 - High Scores

--- high-scores/high_scores.rb
+++ high-scores/high_scores.rb
@@ -16,16 +16,11 @@ class HighScores
   end
 
   def personal_top_three
-    score_sort.take(3)
+    scores.max(3)
   end
 
   def latest_is_personal_best?
     personal_best == latest
   end
 
-  private
-
-  def score_sort
-    @scores.sort{ |a, b| b <=> a }
-  end
 end
