49fa0915c457bfaa0898bc7bdf30cf96538b49d4
HenriqueMariano25 <you@example.com>
2019-11-12 11:46:00 -0300

    Ultima atualização dos arquivos

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -1,15 +1,7 @@
 class Matrix
-  attr_reader :rows
+  attr_reader :rows, :columns
   def initialize(string)
-    @rows = string.split("\n")
-                  .reduce([]){ | result, row | result << row.split.map(&:to_i) }
-  end
-
-  def columns
-    transposed = []
-    (0...rows[0].size).each do |i|
-      transposed << rows.each_with_object([]) { |row, column| column << row[i] }
-    end
-    transposed
+    @rows = string.each_line.map{ |line| line.split.map(&:to_i)}
+    @columns = rows.transpose
   end
 end
